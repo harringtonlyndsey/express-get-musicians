6396938dac4c2677a9a8c871364464cc71d1a73f
harringtonlyndsey <lyndsey.harrington@vodafone.com>
2023-11-07 14:12:26 +0000

    part 3

--- src/app.js
+++ src/app.js
@@ -5,10 +5,37 @@ const { db } = require("../db/connection");
 
 const port = 3000;
 
-//TODO: Create a GET /musicians route to return all musicians
 app.get("/musicians", async (req, res) => {
   const musicians = await Musician.findAll();
   res.json(musicians);
+});
+
+app.get("/musicians/:id", async (req, res) => {
+  const number = req.params.id;
+  const musician = await Musician.findByPk(number);
+  res.json(musician);
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/musicians", async (req, res) => {
+  const newMusician = await Musician.create(req.body);
+  res.json(newMusician);
+});
+
+app.put("/musicians/:id", async (req, res) => {
+  const updatedMusc = await Musician.update(req.body, {
+    where: { id: req.params.id },
+  });
+  res.json(updatedMusc);
+});
+
+app.delete("/musicians/:id", async (req, res) => {
+  const deletedMusc = await Musician.destroy({
+    where: { id: req.params.id },
+  });
+  res.json(deletedMusc);
 });
 
 module.exports = app;
